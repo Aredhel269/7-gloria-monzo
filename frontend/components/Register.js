9cea5a6554e60b8b4273782f0c0e7de4d323464e
Glòria Monzó <dark.aredhel@gmail.com>
2024-04-16 18:14:21 +0200

    login & register

---
+++ frontend/components/Register.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    pass: "",
+  });
+
+  const [error, setError] = useState(""); // Estat per emmagatzemar errors
+  const [response, setResponse] = useState(""); // Estat per emmagatzemar la resposta
+
+  const { name, pass } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/gettoken", {
+        name,
+        pass,
+      });
+
+      console.log(res.data);
+      setResponse(res.data); // Emmagatzema la resposta a l'estat local
+    } catch (err) {
+      console.error(err.response.data);
+      setError(err.response.data.error); // Emmagatzema l'error a l'estat local
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            name="pass"
+            value={pass}
+            onChange={(e) => onChange(e)}
+            minLength="6"
+            required
+          />
+        </div>
+        <input type="submit" value="Register" />
+      </form>
+      {error && <p>{error}</p>} {/* Mostra l'error si hi ha algun */}
+      {response && <p>{response.message}</p>} {/* Mostra la resposta si hi ha algun */}
+    </div>
+  );
+};
+
+export default Register;
---
+++ frontend/pages/_app.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
