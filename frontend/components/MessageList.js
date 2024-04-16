3d2596efcf3e70e5b00c111204257ccf8d734e77
Glòria Monzó <dark.aredhel@gmail.com>
2024-04-16 13:40:21 +0200

    messageList

--- frontend/components/MessageList.js
+++ frontend/components/MessageList.js
@@ -3,10 +3,12 @@ import React from 'react';
 const MessageList = ({ messages }) => {
   return (
     <div>
-      <h2>Llista de Missatges</h2>
+      <h2>Missatges</h2>
       <ul>
         {messages.map((message, index) => (
-          <li key={index}>{message}</li>
+          <li key={index}>
+            <strong>{message.owner}:</strong> {message.text}
+          </li>
         ))}
       </ul>
     </div>
