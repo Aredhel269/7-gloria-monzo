bc73cb749830081226b81387dfd3f7057864add3
Glòria Monzó <dark.aredhel@gmail.com>
2024-04-16 14:17:38 +0200

    socket connect

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,42 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import RoomList from '../components/RoomList'; // Importem el nostre component RoomList
-import MessageList from '../components/MessageList'
+import MessageList from '../components/MessageList';
+import MessageInput from '../components/MessageInput';
 
 const ChatApp = () => {
   // Estats per emmagatzemar la llista de sales i els missatges
   const [rooms, setRooms] = useState([]);
   const [messages, setMessages] = useState([]);
+  const [currentRoom, setCurrentRoom] = useState(null)
+  const socket = io('http://localhost:4000')
 
   // Connexió al servidor de xat en carregar la pàgina
   useEffect(() => {
-    const socket = io('http://localhost:4000'); // Canvia la URL al servidor de xat
     // Lògica per a rebre i gestionar els missatges del servidor
-    socket.on('message', (message)=> {
-      setMessages([...messages, message]);
+    socket.on('update-rooms', (updateRooms)=> {
+      setRooms(updateRooms);
     })
-    socket.on('roomList', (roomList)=> {
-      setRooms(roomList);
+    socket.on('receive-message', (message)=> {
+      setMessages((prevMessages)=> [...prevMessages, message]);
     })
     // Retorna una funció per a fer tasques de neteja en desconnectar el component
     return () => {
       socket.disconnect(); // Desconnecta del servidor de xat
     };
-  }, [messages]);
+  }, []);
 
   // Funció per enviar missatges
   const sendMessage = (message) => {
     // Lògica per enviar el missatge al servidor
+    socket.emit('send-message', message)
+  };
+
+  const joinRoom = (room)=> {
+    setCurrentRoom(room)
+    socket.emit('join-room', room)
   };
 
   return (
     <div>
       <h1>Xat en temps real</h1>
-      {/* Mostrem el component RoomList */}
+      <ChatList rooms={rooms} joinRoom{joinRoom} />
       <RoomList  rooms={rooms} />
-      {/* Component per a la llista de missatges */}
-      <MessageList messages={messages} />
-      {/* Component per a la caixa d'entrada de missatges */}
+      {currentRoom && <MessageList messages={messages}/>}
+      {currentRoom && <MessageInput sendMessage={sendMessage}/>}
     </div>
   );
 };
