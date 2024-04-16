7d8e67fbfcbf91f2501a9dc6cb46ac4d77f99c84
Glòria Monzó <dark.aredhel@gmail.com>
2024-04-16 13:48:52 +0200

    index

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import RoomList from '../components/RoomList'; // Importem el nostre component RoomList
+import MessageList from '../components/MessageList'
 
 const ChatApp = () => {
   // Estats per emmagatzemar la llista de sales i els missatges
@@ -11,12 +12,17 @@ const ChatApp = () => {
   useEffect(() => {
     const socket = io('http://localhost:4000'); // Canvia la URL al servidor de xat
     // Lògica per a rebre i gestionar els missatges del servidor
-
+    socket.on('message', (message)=> {
+      setMessages([...messages, message]);
+    })
+    socket.on('roomList', (roomList)=> {
+      setRooms(roomList);
+    })
     // Retorna una funció per a fer tasques de neteja en desconnectar el component
     return () => {
       socket.disconnect(); // Desconnecta del servidor de xat
     };
-  }, []);
+  }, [messages]);
 
   // Funció per enviar missatges
   const sendMessage = (message) => {
@@ -27,8 +33,9 @@ const ChatApp = () => {
     <div>
       <h1>Xat en temps real</h1>
       {/* Mostrem el component RoomList */}
-      <RoomList />
+      <RoomList  rooms={rooms} />
       {/* Component per a la llista de missatges */}
+      <MessageList messages={messages} />
       {/* Component per a la caixa d'entrada de missatges */}
     </div>
   );
